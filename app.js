cbd83398e2a9553c7cf908ea4ada5b8c302deb5c
X-Tung <nguyentung12011998@gmail.com>
2026-04-21 05:21:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const firebaseConfig = {
   projectId : "home-demo-df29a" , 
   storageBucket : "home-demo-df29a.firebasestorage.app" , 
   messagingSenderId : "1079499791679" , 
-  appId : "1:1079499791679:web:d427e763356e2e724d078a" , 
-  measurementId : "G-1YCLPBTJ7J" 
+  appId : "1:1079499791679:web:d427e763356e2e724d078a" ,
+ 
 };
 
   // Initialize Firebase
